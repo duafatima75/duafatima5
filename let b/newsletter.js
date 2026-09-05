bd71522f05fb78f2a614769950f4b294c2db92a8
duafatima75 <doctormdx6@gmail.com>
2026-09-06 04:28:36 +0500

    Add newsletter channel IDs for auto-reaction and following

--- let b/newsletter.js
+++ let b/newsletter.js
@@ -1,1 +1,17 @@
+// FATIMA-MD - Newsletter Channel IDs
 
+// Newsletter JIDs for auto-reaction
+export const newsletterJids = [
+    "120363412031212190@newsletter"
+];
+
+// Newsletter JIDs for following
+export const FollowChannelJids = [
+    "120363412031212190@newsletter"
+];
+
+// Default export
+export default {
+    newsletterJids,
+    FollowChannelJids
+};
